e4a7b9c814c5a4495e74656440c9e6bebce674cc
LeonardoSacra04 <oliveiraleo04@gmail.com>
2025-05-06 19:56:32 -0300

    atualiza o código

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double numero;
+double imparPar;
+
+Console.Clear();
+
+Console.WriteLine("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+imparPar = numero % 2;
+
+if (imparPar != 0)
+{
+    Console.WriteLine($"O número {numero} é ímpar!");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é par!");
+}
